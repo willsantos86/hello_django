8ae82d67f11a17eb09914204aa4fbad086b2b21d
William Santos <willsantos.edf@gmail.com>
2022-11-19 19:01:24 -0300

    Commit d primeira aplicação do projeto django hello

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def hello(request, nome, idade):
-    return HttpResponse('<h1>Hello {} de {} anos.</h1>'.format(nome, idade))
+def soma(request, numero1, numero2):
+    resultado = numero1 + numero2
+
+    return HttpResponse('<h1>A soma {} + {} = {}.</h1>'.format(numero1, numero2, resultado))
 
--- hello_django/urls.py
+++ hello_django/urls.py
@@ -19,5 +19,5 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>/<int:idade>', views.hello)
+    path('soma/<int:numero1>/<int:numero2>', views.soma)
 ]
